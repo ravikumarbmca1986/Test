8369d0da7d3c81bd9f4e01584d76aed2f8a82c15
PPMS <PPMS@TA-RAVIKUMAR>
2017-12-27 12:53:21 +0530

    rdgtfd

--- Test/src/AES.java
+++ Test/src/AES.java
@@ -12,7 +12,7 @@ public class AES {
 	private static String mykey = "***** this is the secret key hidden *****";
     private static SecretKeySpec secretKey;
     private static byte[] key;
-   //ravi is coming
+   //ravi is coming  1
  
     public static void setKey(String myKey)
     {
